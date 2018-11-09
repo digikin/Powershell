c3c7590f5d6af081eceacad9d9b608e128838def
eric stumbo <ericstumbo@student.purdueglobal.edu>
2018-11-08 19:20:31 -0500

    added user input for queue number

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Enabling or Disabling Windows Optional features based on their state.
 
 This has to be run in an elevated state. 
+
+*We can configure this later to install the local printer through the network.
+
+The way it works is once a USB printer has been connectd by USB and set it will copy its port name to add the 
+LPR/LPD printer.  
--- winfeatures.ps1
+++ winfeatures.ps1
@@ -3,7 +3,10 @@ $M = (Get-WindowsOptionalFeature -Online | where FeatureName -eq Printing-Founda
 if ($L.state -eq "disabled"){enable-windowsoptionalfeature -Online -FeatureName "Printing-Foundation-LPDPrintService"}
 if ($M.state -eq "disabled"){enable-windowsoptionalfeature -Online -FeatureName "Printing-Foundation-LPRPortMonitor"}
 
-$queue = 'Put queue # here"
+write-host "Features Enabled"
+
+$queue = read-host -prompt "What is the queue #"
+
 $Q = get-printer -Name *HP*
 add-printer -name $queue -portname $Q.portname -drivername "Generic / Text Only"
 
