59b7c96169284fc76796a26b110233e5068fa056
eric stumbo <ericstumbo@student.purdueglobal.edu>
2018-11-09 06:15:52 -0500

    created a no share function

--- winfeatures.ps1
+++ winfeatures.ps1
@@ -11,6 +11,14 @@ $queue = read-host -prompt "What is the queue #"
 $Q = get-printer -Name *HP*
 add-printer -name $queue -portname $Q.portname -drivername "Generic / Text Only"
 
+$printers = Get-Wmi-WMIObject -class Win32_Printer -computer localhost
+foreach ($printer in $printers){
+	IF($Printer.PortName.Contains("USB")-and $printer.published -eq $true){
+		$printer.Name + ":" + $printer.published
+		$printer.published = $false
+		$printer.put()
+	}
+}
 
 
 
